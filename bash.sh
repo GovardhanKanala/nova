8795a87d47476729d4072730cc96ea5c84e808ff
GovardhanKanala <GovardhanKanala@users.noreply.github.com>
2024-05-14 09:03:44 +0000

    Publish sample file to GitHub Pages e3df33f7f1d66ea0cb0bd92dc1356b55a305251a

--- bash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create folders a to f
-for folder in {a..f}; do
-    mkdir $folder
-    echo "Folder $folder created"
-    echo "<!DOCTYPE html><html><head><title>Index</title></head><body><h1>Current folder is $folder</h1></body></html>" > $folder/index.html
-done
